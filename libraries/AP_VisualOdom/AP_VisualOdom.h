a70b5cbc95e71a9e76723e4185a9cf1cc6e8473a
chobits <chobits@itri.org.tw>
2020-05-15 09:05:12 +0800

    AP_VisualOdom: add function comments

--- libraries/AP_VisualOdom/AP_VisualOdom.h
+++ libraries/AP_VisualOdom/AP_VisualOdom.h
@@ -79,6 +79,8 @@ public:
     void handle_vision_position_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, float roll, float pitch, float yaw, uint8_t reset_counter);
     void handle_vision_position_estimate(uint64_t remote_time_us, uint32_t time_ms, float x, float y, float z, const Quaternion &attitude, uint8_t reset_counter);
     
+    // general purpose methods to consume velocity estimate data and send to EKF
+    // velocity in NED meters per second
     void handle_vision_speed_estimate(uint64_t remote_time_us, uint32_t time_ms, const Vector3f &vel);
 
     // calibrate camera attitude to align with vehicle's AHRS/EKF attitude
